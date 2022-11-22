42fb815c967ac30a059f4bb58ecf2d25c037b5d7
mj6meron <meronmkl@gmail.com>
2022-11-22 17:31:11 +0100

    finished with cv app

--- CV-app/app/src/main/java/com/example/cv_app/MainActivity.kt
+++ CV-app/app/src/main/java/com/example/cv_app/MainActivity.kt
@@ -2,24 +2,45 @@ package com.example.cv_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val sentences = mutableListOf("sentence 1")
-        sentences += ("sentence 2")
-        sentences += ("sentence 3")
-        sentences += ("sentence 4")
-        sentences += ("sentence 5")
-        sentences += ("sentence 6")
-        sentences += ("sentence 7")
-        sentences += ("sentence 8")
-        sentences += ("sentence 9")
-        sentences += ("sentence 10")
-        print (sentences)
+        // Importing
+        var switchButton = findViewById<Button>(R.id.switchButton)
+        var generatedText = findViewById<TextView>(R.id.generatedText)
+        var imageView = findViewById<ImageView>(R.id.imageView)
+        switchButton.setOnClickListener {
 
+            var myText = generateSentence()
+            generatedText.text = myText
 
+            if (myText == "I like Messi!"){
+                imageView.visibility = View.VISIBLE
+            } else {
+                imageView.visibility = View.INVISIBLE
+            }
+
+        }
+    }
+
+    fun generateSentence(): String{
+        val sentences = mutableListOf("My name is Meron Habtemichael")
+        sentences += ("I am a software development student")
+        sentences += ("I live in Kristianstad")
+        sentences += ("I like mobile development but more of cross platform than Native")
+        sentences += ("I started programming when I was in highschool")
+        sentences += ("I am passionate about Machine Learning!")
+        sentences += ("I play basketball in my free time")
+        sentences += ("I have lived in stockholm before coming to kristianstad")
+        sentences += ("I am originally from Eritrea")
+        sentences += ("I like Messi!")
+        return (sentences[(0..9).random()])
     }
 }
